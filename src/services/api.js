6967ce03cc966f40eb9077f44ade30ebccccb4f6
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-09-03 18:47:32 +0530

    updated the base url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://assistify-backend-n646.onrender.com/" || "http://localhost:3000/api",
+  baseURL: "http://assistify-backend-n646.onrender.com/api" || "http://localhost:3000/api",
 });
 
 // Attach token automatically
