f2e5c0a50134aaa3683ba2ca4e20e8443cd3be21
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-09-04 16:24:30 +0530

    api url updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://assistify-backend-n646.onrender.com/api'
 });
 // Attach token automatically
 API.interceptors.request.use((config) => {
